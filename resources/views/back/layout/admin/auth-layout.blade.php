bac3da6b1ca3e696498dcf9ebbe44088f8a700f4
Player Biasa <adhie.cahyono@gmail.com>
2023-09-18 16:26:48 +0700

    re push

--- resources/views/back/layout/admin/auth-layout.blade.php
+++ resources/views/back/layout/admin/auth-layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Login &mdash; Stisla</title>
+    <title>Admin Login</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
@@ -28,22 +28,31 @@
                     <div
                         class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                         <div class="login-brand"> </div>
-                        <h4 class="text-dark font-weight-normal" style="text-align: center">Welcome to <span
-                                class="font-weight-bold">Stisla</span>
+                        <h4 class="text-dark font-weight-normal" style="text-align: center">Admin <span
+                                class="font-weight-bold">Login</span>
                         </h4>
+                        <hr>
+                        @if (Session::get('fail'))
+                            <div class="alert alert-danger">
+                                {{ Session::get('fail') }}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @endif
                         <div class="card card-primary">
                             <div class="card-body">
-                                <form method="POST" action="#" class="needs-validation" novalidate=""
-                                    autocomplete="off">
+                                <form method="POST" action="#" class="needs-validation" autocomplete="off">
                                     <div class="form-group">
-                                        <label for="email">Email or NIM</label>
+                                        <label for="email">Email/Username</label>
                                         <input id="login_id" type="text" class="form-control" name="login_id"
-                                            tabindex="1" required autofocus>
-                                        <div class="invalid-feedback">
-                                            Please fill in your email
-                                        </div>
+                                            value="{{ old('login_id') }}" tabindex="1" autofocus>
                                     </div>
-
+                                    @error('login_id')
+                                        <div class="d-block text-danger" style="margin-top: -25px;margin-bottom: 15px;">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                     <div class="form-group">
                                         <div class="d-block">
                                             <label for="password" class="control-label">Password</label>
@@ -54,12 +63,13 @@
                                             </div>
                                         </div>
                                         <input id="password" type="password" class="form-control" name="password"
-                                            tabindex="2" required>
-                                        <div class="invalid-feedback">
-                                            please fill in your password
-                                        </div>
+                                            tabindex="2">
                                     </div>
-
+                                    @error('password')
+                                        <div class="d-block text-danger" style="margin-top: -25px;margin-bottom: 15px;">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                     <div class="form-group">
                                         <div class="custom-control custom-checkbox">
                                             <input type="checkbox" name="remember" class="custom-control-input"
--- routes/admin.php
+++ routes/admin.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->name('admin.')->group(function(){
 
     Route::middleware(['guest:admin'])->group(function(){
-
+        Route::view('/login', 'back.layout.admin.auth-layout')->name('login');
     });
 
     Route::middleware(['auth:admin'])->group(function(){
-
+        Route::view('/home', 'back.pages.admin.home')->name('home');
     });
 });
